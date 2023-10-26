90197489c9c413aba3c9403bf9b629dc509bcde8
JhonAnthonyGiron <jagiron@utpl.edu.ec>
2023-10-26 17:18:51 -0500

    Resolucion segundo ejercicio

--- Ejercicio02/src/ejercicio02/Ejercicio02.java
+++ Ejercicio02/src/ejercicio02/Ejercicio02.java
@@ -1,28 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ejercicio02;
-
-/**
- *
- * @author reroes
- */
 public class Ejercicio02 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // Resolución del problema
         int var1 = 10;
         int var2 = 2;
+        double respuesta;
         
-                
+        System.out.println("-------Solución a problema------------------");
+        // Math.pow me permite elevar una base por un exponente
+        
+        respuesta = Math.pow(var1, 2)+10;
+        System.out.println("La respuesta es " + respuesta);
+        
+         //Ejercicio Incompleto, Realizado por Jhon Giron
+        }
     }
-    
-    
-    
-    
-}
