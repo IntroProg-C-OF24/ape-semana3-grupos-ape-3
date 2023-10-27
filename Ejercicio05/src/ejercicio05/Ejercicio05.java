4cc814a75a561b183b2efa04c7b73d73066c8dd6
JhonAnthonyGiron <jagiron@utpl.edu.ec>
2023-10-27 14:37:34 -0500

    ejercicio4

--- Ejercicio05/src/ejercicio05/Ejercicio05.java
+++ Ejercicio05/src/ejercicio05/Ejercicio05.java
@@ -1,18 +1,17 @@
-package ejercicio03;
-public class Ejercicio03 {
+package ejercicio05;
+public class Ejercicio05 {
     public static void main(String[] args) {
         //Resolucion del problema
-        int var1 = 10;
-        int var2 = 16;
-        int var3 = 20;
-        int var4 = 1;
-        int var5 = 11;
+        int var1 = 2;
+        int var2 = 12;
+        int var3 = 1;
+        int var4 = 3;
         double respuesta;
         
         System.out.println("-----Solucion del problema-----");
         
-        respuesta = Math.pow (var1, 2) + Math.sqrt(var2) + (var3 / (var4+var5));
-        
+        respuesta = (Math.pow(var1, 2)+var2-var3)/3;
+         
         System.out.println("La respuesta es " + respuesta);
         
         //Ejercicio resulto por Jhon Giron
